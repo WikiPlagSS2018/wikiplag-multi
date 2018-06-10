45ba36d2956ea247487ce9a1975f3d69bbb3f99a
BaconCookie <laurahartgers@live.nl>
2018-06-10 11:58:07 +0200

    add class for w2v plag finder and corresponding tests

---
+++ plagFinderWord2Vec/src/main/scala/de/htwberlin/f4/wikiplag/plagFinderWord2Vec/foo.java
@@ -0,0 +1,6 @@
+package de.htwberlin.f4.wikiplag.plagFinderWord2Vec;
+
+
+
+public class foo {
+}
---
+++ plagFinderWord2Vec/src/test/scala/de/htwberlin/f4/wikiplag/plagFinderWord2Vec/PlagiarismFinderW2VTest.scala
@@ -0,0 +1,10 @@
+package de.htwberlin.f4.wikiplag.plagFinderWord2Vec
+
+import org.junit.Assert._
+import org.junit._
+import org.scalatest.junit.AssertionsForJUnit
+
+class PlagiarismFinderW2VTest extends AssertionsForJUnit {
+
+
+}
